5b39d0fba1d7fbab3c506c8987442b37bed1b9b9
Giovanni Favorin de Melo <Giovannifavorim@gmail.com>
2024-09-25 09:08:58 -0300

    app

--- App.js
+++ App.js
@@ -1,20 +1,98 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timer: null,
+      number: 0,
+      startStopText: 'Start',
+    };
+
+    this.startStopButton = this.startStopButton.bind(this);
+    this.clearButton = this.clearButton.bind(this);
+  }
+
+  startStopButton() {
+    if (this.state.timer === null) {
+      this.setState({
+        startStopText: 'Stop',
+        timer: setInterval(() => {
+          this.setState((prevState) => ({
+            number: prevState.number + 0.1,
+          }));
+        }, 100),
+      });
+    } else {
+      clearInterval(this.state.timer);
+      this.setState({
+        startStopText: 'Start',
+        timer: null,
+      });
+    }
+  }
+
+  clearButton() {
+    clearInterval(this.state.timer);
+    this.setState({
+      number: 0,
+      startStopText: 'Start',
+      timer: null,
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.body}>
+        <View style={styles.tContainer}>
+          <Text style={styles.counterText}>{this.state.number.toFixed(1)}</Text>
+
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={this.startStopButton}>
+              <Text style={styles.buttonText}>{this.state.startStopText}</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.button} onPress={this.clearButton}>
+              <Text style={styles.buttonText}>Clear</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f5fcff",
+  },
+  tContainer: {
+    alignItems: "center",
+  },
+  counterText: {
+    fontSize: 48,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "60%",
+  },
+  button: {
+    padding: 10,
+    backgroundColor: "#007AFF",
+    borderRadius: 5,
+    margin: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    textAlign: "center",
   },
 });
